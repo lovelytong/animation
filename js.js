64568ae50b4729c8f0487fdba9d5f677a743f9d1
lovelytong <tongyu@tongyudeMacBook-Air.local>
2018-02-12 10:36:56 +0800

    study-animation

---
+++ js.js
@@ -0,0 +1,13 @@
+function positionMessage() {
+  if(!document.getElementById) return false;
+  if(!document.getElementById('message')) return false;
+  var elem = document.getElementById('message');
+  elem.style.position = "absolute";
+  elem.style.left = "0px";
+  elem.style.top = "0px";
+  moveElement('message',200,200,50);
+}
+
+addLoadEvent(positionMessage);
+
+
