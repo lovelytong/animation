5bd1eb25c1ec1d1210f8df458652c695dbfd2313
lovelytong <tongyu@tongyudeMacBook-Air.local>
2018-03-28 23:26:18 +0800

    跳来跳去

--- js.js
+++ js.js
@@ -2,10 +2,14 @@ function positionMessage() {
   if(!document.getElementById) return false;
   if(!document.getElementById('message')) return false;
   var elem = document.getElementById('message');
+  var father = document.getElementById('father')
   elem.style.position = "absolute";
   elem.style.left = "0px";
   elem.style.top = "0px";
-  moveElement('message',5);
+  father.style.position = "absolute";
+  father.style.left = "30px";
+  father.style.top = "30px";
+  moveElement('father','message',20);
 }
 
 addLoadEvent(positionMessage);
